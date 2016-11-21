e66f4baaf7f41fa7c423649041d67c179b35ff5b
Rory Nolan <rorynoolan@gmail.com>
2016-11-21 08:49:55 +0000

    Use java 6 for mac rJava compatibility.

--- R/thresh.R
+++ R/thresh.R
@@ -6,11 +6,7 @@
 #' "Otsu", "Percentile", "RenyiEntropy", "Shanbhag", "Triangle", "Yen". Read
 #' about them at \url{http://imagej.net/Auto_Threshold}.
 #'
-#' @param img_mat An array of \emph{integers} with a "bits" attribute, where
-#'   theis must be 8 or 16. Note that \code{\link[EBImage]{EBImage}}'s
-#'   \code{\link[EBImage]{display}} does not read in images as integers, nor
-#'   does it give them a "bits" attribute, but the
-#'   \code{\link[nandb]{ReadImageData}} function from the nandb package (\url{http://github.com/rorynolan/nandb}) does these things.
+#' @param img_mat An array of \emph{integers}.
 #' @param method The full name (with correct case) of the method you wish to use
 #'   (e.g. "Huang").
 #'
@@ -27,21 +23,14 @@
 #'
 #' @export
 auto_thresh <- function(img_mat, method) {
-  if (!CanBeInteger(img_mat)) {
-    stop("img_mat must be a mtrix of integers.")
+  if ((!CanBeInteger(img_mat)) || any(img_mat < 0)) {
+    stop("img_mat must be a mtrix of non-negative integers.")
   }
-  bits <- attr(img_mat, "bits")
-  if (is.null(bits)) {
-    stop("img_mat must have a 'bits' attribute.")
-  }
-  if (! bits %in% c(8, 16)) {
-    stop("This function only works on 8- or 16-bit images.")
-  }
-  im_hist <- sapply(seq_len(2 ^ bits) - 1, function(x) {
+  im_hist <- sapply(0:max(img_mat), function(x) {
     sum(x == img_mat)
   })
-  at_class <- rJava::.jnew("Auto_Threshold")
-  rJava::.jcall(at_class, "I", method, im_hist)
+  autothresh_class <- rJava::.jnew("Auto_Threshold")
+  rJava::.jcall(autothresh_class, "I", method, im_hist)
 }
 
 #' @rdname auto_thresh
